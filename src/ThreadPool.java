0eaf46e8c5f92bac1b1ea41a2ca3614ac8990085
lvgo <lvgorice@gmail.com>
2019-09-24 11:58:19 +0800

    草案

--- src/ThreadPool.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author lvgo
- * @version 1.0
- * @package PACKAGE_NAME
- * @description: //TODO 一句话描述此类作用
- * @date 2019/9/24 9:05
- */
-public class ThreadPool {
-}
---
+++ src/test/java/com/TestThreadPool.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author lvgo
+ * @version 1.0
+ * @package com
+ * @description: //TODO 一句话描述此类作用
+ * @date 2019/9/24 11:42
+ */
+public class TestThreadPool {
+
+    @Test
+    public void test(){
+
+    }
+}
